13fa869102faba33231d1f908fd4bdb6fbe2e5f8
KotenkoNata <kotenkonata8@gmail.com>
2022-04-26 16:23:05 +0300

    Done: extended-repeater.js vigenere-cipher.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
@@ -15,10 +15,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// export default
+function repeater(str = isRequired(''),
+                  options= {separator: '+', additionSeparator: '|' }) {
+
+  console.log(str, options)
+
+  //check data
+
+  if(typeof str !== "string"){
+    str = toString(str);
+  }
+
+  if(typeof options.addition !== 'string'){
+    options.addition = toString(options.addition);
+  }
+
+  if(!options.repeatTimes){
+    options.repeatTimes = 1;
+  }
+
+  if(!options.additionRepeatTimes){
+    options.additionRepeatTimes = 0;
+  }
+
+  //
+
+  let result = '';
+
+  for(let i = 0; i < options.repeatTimes; i++) {
+
+    result = result+str;
+
+    if(options.additionRepeatTimes){
+
+      for (let j = 0; j < options.additionRepeatTimes; j++){
+
+        if(options.additionSeparator){
+          result = result + options.addition + options.additionSeparator;
+        }
+      }
+
+      if(options.additionSeparator){
+        result = result.slice(0, result.length - options.additionSeparator.length)
+      }
+
+    }else if(!options.additionRepeatTimes && options.addition){
+      result = result+options.addition;
+    }
+
+    if(options.separator){
+      result = result + options.separator;
+    }
+
+
+  }
+
+  result = result.slice(0, result.length - options.separator.length)
+
+  return result;
 }
+
+// console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }))
+
+//'STRING PLUS 00 PLUS00 PLUS** STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+//str + options.addition + options.additionSeparator
+
+// console.log(repeater('la', { repeatTimes: 3 }))
+
+// console.log(repeater('la', { repeatTimes: 3, separator: 's', addition: '+', additionRepeatTimes: 1 }))
+  //'la+sla+sla+'
+
+// console.log(repeater('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' }))
+//'TESTstrADD!'
 
 module.exports = {
   repeater
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,34 +1,195 @@
-const { NotImplementedError } = require('../extensions/index.js');
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+export default class VigenereCipheringMachine {
+
+  _direct = true
+
+  constructor(direct) {
+    if (arguments.length >= 1) {
+      this._direct = !!direct
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (arguments.length < 2) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (!message || message.length < 1) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (!key || key.length < 1) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = '';
+
+    let keyCharsCodes = [];
+
+    message = message.toLowerCase();
+    key = key.toLowerCase()
+
+    for (let j = 0; j < key.length; j++) {
+      keyCharsCodes.push((key[j].charCodeAt() - 97) % 26)
+    }
+
+    let words = message.split(' ');
+
+    let keyCharIndex = 0;
+
+    let cipher_i;
+
+    for (let word = 0; word < words.length; word++) {
+
+      for (let wordCharIndex = 0; wordCharIndex < words[word].length; wordCharIndex++) {
+
+        let shift = keyCharsCodes[keyCharIndex % keyCharsCodes.length];
+
+        let charCode = words[word][wordCharIndex].charCodeAt();
+
+        if (charCode < 97 || charCode > 122) {
+          result += words[word][wordCharIndex];
+          continue;
+        }
+
+        cipher_i = (charCode - 97 + shift) % 26;
+
+        result += String.fromCharCode(97 + cipher_i);
+
+        keyCharIndex = keyCharIndex + 1;
+
+
+        if (keyCharIndex > keyCharsCodes.length - 1) {
+          keyCharIndex = 0;
+        }
+      }
+
+
+      result += ' '
+    }
+    result = result.toUpperCase().trim()
+    if (!this._direct) {
+      result = result.split('').reverse().join('')
+    }
+    return result
+  }
+
+  decrypt(encryptedMessage, key) {
+
+    if (arguments.length < 2) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    if (!encryptedMessage) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (!key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    if (encryptedMessage.length < 1) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (key.length < 1) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = '';
+
+    let keyCharsCodes = [];
+
+    encryptedMessage = encryptedMessage.toLowerCase();
+    key = key.toLowerCase();
+
+    // if (!this._direct) {
+    //   encryptedMessage = encryptedMessage.split('').reverse().join('')
+    // }
+
+    for (let j = 0; j < key.length; j++) {
+      keyCharsCodes.push((key[j].charCodeAt() - 97) % 26)
+    }
+
+    let words = encryptedMessage.split(' ');
+
+    let keyCharIndex = 0;
+
+    let cipher_i;
+
+    for (let word = 0; word < words.length; word++) {
+
+      for (let wordCharIndex = 0; wordCharIndex < words[word].length; wordCharIndex++) {
+
+        let shift = keyCharsCodes[keyCharIndex % keyCharsCodes.length];
+
+        let charCode = words[word][wordCharIndex].charCodeAt();
+
+        if (charCode < 97 || charCode > 122) {
+
+          result += words[word][wordCharIndex];
+
+          continue;
+        }
+
+        cipher_i = (charCode - 97 - shift) % 26;
+
+        if (cipher_i < 0) {
+          cipher_i = 26 + cipher_i;
+        }
+
+        result += String.fromCharCode(97 + cipher_i);
+
+        keyCharIndex = keyCharIndex + 1;
+
+        if (keyCharIndex > keyCharsCodes.length - 1) {
+          keyCharIndex = 0;
+        }
+      }
+
+      result += ' '
+    }
+
+
+    if (!this._direct) {
+      result = result.split('').reverse().join('')
+    }
+    return result.toUpperCase().trim()
   }
 }
+
+
+// const directMachine = new VigenereCipheringMachine();
+// console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
+//
+// const reverseMachine = new VigenereCipheringMachine(false);
+// console.log(reverseMachine.encrypt('attack at dawn!', 'alphonse'))
+// => '!ULLD XS XQHIEA')
+// => 'AEIHQX SX DLLU!'
+
+// const directMachine = new VigenereCipheringMachine();
+
+// console.log(directMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
+
+// console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
+// => 'AEIHQX SX DLLU!'
+// const reverseMachine = new VigenereCipheringMachine(false);
 
 module.exports = {
   VigenereCipheringMachine
